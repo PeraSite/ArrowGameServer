531b7d1a4ab9bce69b1c08a4ba59df25872f5eb4
PeraSite <pretocki3@gmail.com>
2023-05-27 19:18:28 +0900

    chore: 서버 클래스 생성

---
+++ Server/GameServer.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ArrowGame.Server;
+
+public class GameServer : IDisposable {
+	private readonly TcpListener _server;
+
+	public GameServer(int listenPort) {
+		_server = new TcpListener(IPAddress.Any, listenPort);
+	}
+
+	public void Start() {
+		Console.WriteLine($"[TCP 서버] 서버 시작@{_server.LocalEndpoint}");
+		_server.Start();
+	}
+
+	public void Dispose() {
+		Console.WriteLine("[TCP 서버] 서버 종료");
+		_server.Stop();
+		GC.SuppressFinalize(this);
+	}
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
 
-Console.WriteLine("Hello, World!");
+namespace ArrowGame.Server;
+
+internal static class Program {
+	private static void Main() {
+		// 콘솔 입출력 한글 깨짐 수정
+		Console.InputEncoding = Encoding.Unicode;
+		Console.OutputEncoding = Encoding.Unicode;
+
+		// 환경변수 가져오기
+		var listenPort = int.Parse(GetEnvironmentVariable("LISTEN_PORT"));
+
+		// 서버 시작
+		using GameServer server = new GameServer(listenPort);
+		server.Start();
+	}
+
+	private static string GetEnvironmentVariable(string key)
+		=> Environment.GetEnvironmentVariable(key) ?? throw new Exception($"{key} is not set");
+}
